b2f25dd9b31c6ae95c3061a1f845b1c99095586c
jsoler88 <33063331+jsoler88@users.noreply.github.com>
2017-10-25 14:11:34 -0500

    add formulario

--- resources/views/form_category.blade.php
+++ resources/views/form_category.blade.php
@@ -1,25 +1,11 @@
 @include('header')
 <div>
-	<form action="{{ url('/email_contact') }}" method="POST" role="form">
-      <div class="form-group">
-        <input type="text" id="InputName" name="InputName" placeholder="Nombre">
-      </div>
+	<form>
+   <label for="Inputcategoria">Escriba la categoria: </label>
+   <input type="text" name="Inputcategoria" id="Inputcategoria" placeholder="Escriba el tipo de categoria">
+   <input type="submit" name="inputenviar" id="Imputenviar" value="Enviar">
 
-      <div class="form-group">
-        <input type="text" id="InputEmail" name="InputEmail" placeholder="Correo">
-      </div>
-      <input type="hidden" name="_token" value="{{ csrf_token() }}">
-      <div class="row">
-        <div class="col-md-12">
-          <center><div class="g-recaptcha" data-sitekey="6LdN5BoUAAAAAGbGcZGmTSlbR2HShhKKy2Zt_Uur"></div></center><br>
-        </div>
-        <div class="col-md-12">
-          <center>
-            <button id="enviar" type="submit" class="btn btn-primary" style="">Enviar mensaje</button>
-            <button id="enviando" type="submit" class="btn btn-success">&nbsp;&nbsp;Enviando...&nbsp;&nbsp;</button>
-          </center>
-        </div>
-      </div>
-    </form>
+
+  </form>
 </div>
 @include('footer')
